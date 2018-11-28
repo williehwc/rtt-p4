da923f3618ef89249dee1225cd935066a373bcf8
Willie Chang <williehwc@gmail.com>
2018-11-28 18:47:03 -0500

    Modify receive.py to send ACK packets. Modify send.py to send another packet after receiving an ACK packet.

--- src/receive.py
+++ src/receive.py
@@ -4,10 +4,10 @@ import struct
 import os
 
 from scapy.all import sniff, sendp, hexdump, get_if_list, get_if_hwaddr
-from scapy.all import Packet, IPOption
-from scapy.all import ShortField, IntField, LongField, BitField, FieldListField, FieldLenField
-from scapy.all import IP, TCP, UDP, Raw
-from scapy.layers.inet import _IPOption_HDR
+from scapy.all import Ether, IP, TCP
+
+this_ip = {"h1-eth0": "10.0.1.1", "h2-eth0": "10.0.2.2"}
+min_port_no = 49152
 
 def get_if():
     ifs=get_if_list()
@@ -21,33 +21,29 @@ def get_if():
         exit(1)
     return iface
 
-class IPOption_MRI(IPOption):
-    name = "MRI"
-    option = 31
-    fields_desc = [ _IPOption_HDR,
-                    FieldLenField("length", None, fmt="B",
-                                  length_of="swids",
-                                  adjust=lambda pkt,l:l+4),
-                    ShortField("count", 0),
-                    FieldListField("swids",
-                                   [],
-                                   IntField("", 0),
-                                   length_from=lambda pkt:pkt.count*4) ]
-def handle_pkt(pkt):
-    if TCP in pkt and pkt[TCP].dport == 1234:
-        print "got a packet"
-        pkt.show2()
-    #    hexdump(pkt)
+def handle_packet(packet):
+    iface = get_if()
+    if (TCP in packet and packet[TCP].dport >= min_port_no
+        and packet[IP].dst == this_ip[iface] and not packet[TCP].flags & 0x04): # RST
+        print "Got a packet"
+        packet.show2()
+        hexdump(packet)
         sys.stdout.flush()
-
+        # Send ACK
+        print "Sending ACK"
+        ack_packet = Ether(src = get_if_hwaddr(iface), dst = 'ff:ff:ff:ff:ff:ff')
+        ack_packet = ack_packet / IP(src = packet[IP].dst, dst = packet[IP].src)
+        ack_packet = ack_packet / TCP(sport = packet[TCP].dport, dport = packet[TCP].sport,
+            flags = 'A', seq = packet[TCP].seq, ack = packet[TCP].seq + len(packet[TCP].load))
+        ack_packet.show2()
+        sendp(ack_packet, iface = iface, verbose = False)
 
 def main():
     ifaces = filter(lambda i: 'eth' in i, os.listdir('/sys/class/net/'))
     iface = ifaces[0]
-    print "sniffing on %s" % iface
+    print "Sniffing on %s" % iface
     sys.stdout.flush()
-    sniff(iface = iface,
-          prn = lambda x: handle_pkt(x))
+    sniff(iface = iface, prn = lambda x: handle_packet(x))
 
 if __name__ == '__main__':
     main()
--- src/send.py
+++ src/send.py
@@ -4,10 +4,16 @@ import sys
 import socket
 import random
 import struct
+import string
+import os
 
-from scapy.all import sendp, send, get_if_list, get_if_hwaddr
-from scapy.all import Packet
-from scapy.all import Ether, IP, UDP, TCP
+from scapy.all import sendp, send, get_if_list, get_if_hwaddr, sniff, hexdump
+from scapy.all import Ether, IP, TCP
+
+this_ip = {"h1-eth0": "10.0.1.1", "h2-eth0": "10.0.2.2"}
+destination_ips = {"h1-eth0": "10.0.2.2", "h2-eth0": "10.0.1.1"}
+num_chars_per_packet = 10
+min_port_no = 49152
 
 def get_if():
     ifs=get_if_list()
@@ -21,21 +27,45 @@ def get_if():
         exit(1)
     return iface
 
+def random_string(length):
+    # Source: https://stackoverflow.com/questions/2257441
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+
+def send_packet(message, seq_no):
+    iface = get_if()
+    address = socket.gethostbyname(destination_ips[iface])
+    print "Sending on interface %s to %s" % (iface, str(address))
+    dport = random.randint(min_port_no, 65535)
+    sport = random.randint(min_port_no, 65535)
+    packet = Ether(src = get_if_hwaddr(iface), dst = 'ff:ff:ff:ff:ff:ff')
+    packet = packet / IP(dst = address) / TCP(dport = dport, sport = sport, seq = seq_no) / message
+    packet.show2()
+    hexdump(packet)
+    sendp(packet, iface = iface, verbose = False)
+
+def handle_packet(packet, max_seq_no):
+    iface = get_if()
+    if (TCP in packet and packet[TCP].dport >= min_port_no
+        and packet[IP].dst == this_ip[iface] and packet[TCP].flags & 0x10): # ACK
+        print "Got an response"
+        packet.show2()
+        hexdump(packet)
+        sys.stdout.flush()
+        # Send next packet
+        next_seq = packet[TCP].ack
+        if next_seq <= max_seq_no:
+            send_packet(random_string(num_chars_per_packet), next_seq)
+
 def main():
 
-    if len(sys.argv)<3:
-        print 'pass 2 arguments: <destination> "<message>"'
-        exit(1)
+    if len(sys.argv) < 2:
+        print 'Pass 1 argument: <max_seq_no>'
+        #exit(1)
 
-    addr = socket.gethostbyname(sys.argv[1])
-    iface = get_if()
+    send_packet(random_string(num_chars_per_packet), 0)
 
-    print "sending on interface %s to %s" % (iface, str(addr))
-    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
-    pkt = pkt /IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[2]
-    pkt.show2()
-    sendp(pkt, iface=iface, verbose=False)
-
+    ifaces = filter(lambda i: 'eth' in i, os.listdir('/sys/class/net/'))
+    sniff(iface = ifaces[0], prn = lambda x: handle_packet(x, int(sys.argv[1])))
 
 if __name__ == '__main__':
     main()

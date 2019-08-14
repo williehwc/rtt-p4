134d778ea0a564b100e2e3eb3a7c6503721a79e5
Willie Chang <williehwc@gmail.com>
2019-08-14 08:21:27 -0400

    compare-pcaps.py bugfixes

--- src/compare-pcaps.py
+++ src/compare-pcaps.py
@@ -1,8 +1,7 @@
-# WORK IN PROGRESS!!!
-# Compared an observed PCAP against a standard PCAP w.r.t. TCP packets
+# Compare an observed PCAP against a standard PCAP w.r.t. TCP packets
 # Only works for observed PCAP that corresponds one-to-one with the standard PCAP
 # For example, if the standard PCAP was replayed twice, do not use this script
-# All "tcp" and "ip" header fields as defined in TSHARK_COMMAND are compared
+# All "tcp" and "ip" header fields as defined in tshark_command are compared
 # Usage: python3 compare-pcaps.py standard.pcap observed.pcap
 
 import sys, subprocess, json, pandas
@@ -14,18 +13,18 @@ def tshark_command(filename):
         "-j", "tcp",
         "-T", "json",
         "-e", "frame.number",
-        "-e", "tcp.stream",
         "-e", "ip.src",
         "-e", "tcp.srcport",
         "-e", "ip.dst",
         "-e", "tcp.dstport",
+        "-e", "ip.len",
+        "-e", "ip.checksum",
+        "-e", "ip.ttl",
         "-e", "tcp.seq",
-        "-e", "tcp.len",
         "-e", "tcp.ack",
-        "-e", "tcp.flags.syn",
-        "-e", "tcp.flags.ack",
-        "-e", "tcp.analysis.ack_rtt",
-        "-e", "tcp.analysis.initial_rtt",
+        "-e", "tcp.flags",
+        "-e", "tcp.checksum",
+        "-e", "tcp.window_size_value",
         "-e", "frame.time_epoch"
     ]
 
@@ -51,10 +50,13 @@ def main():
     del tshark_result
 
     matched_packets = []
+    matched_indices_of_observed_packets = []
+    offset = None
 
     standard_packets = [preprocess_packet(pc) for pc in standard_packet_capture]
     observed_packets = [preprocess_packet(pc) for pc in observed_packet_capture]
     for o, observed_packet in enumerate(observed_packets):
+        print(o)
         for s, standard_packet in enumerate(standard_packets):
             matched = True
             for tc in tshark_command(""):
@@ -62,19 +64,25 @@ def main():
                     try:
                         if observed_packet[tc] != standard_packet[tc]:
                             matched = False
+                            break
                     except:
                         matched = False
                         break
             if matched:
+                if offset is None:
+                    offset = observed_packet["frame.time_epoch"] - standard_packet["frame.time_epoch"]
                 standard_packet["TIMEDIFF"] = \
-                    observed_packet["frame.time_epoch"] - standard_packet["frame.time_epoch"]
+                    observed_packet["frame.time_epoch"] - standard_packet["frame.time_epoch"] - offset
                 matched_packets.append(standard_packet)
-                del observed_packets[o]
+                matched_indices_of_observed_packets.append(o)
                 del standard_packets[s]
                 break
     
+    for o in matched_indices_of_observed_packets[::-1]:
+        del observed_packets[o]
+
     pandas.DataFrame(matched_packets).to_csv("matched_packets.csv")
-    pandas.DataFrame(observed_packets).to_csv("observed_packets.csv")
-    pandas.DataFrame(standard_packets).to_csv("standard_packets.csv")
+    pandas.DataFrame(observed_packets).to_csv("leftover_observed_packets.csv")
+    pandas.DataFrame(standard_packets).to_csv("leftover_standard_packets.csv")
 
 main()
